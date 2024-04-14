9cf149a0546bab64ee356a28ffa488e7b3f0da1b
MaksymB <maksym.bilinets@gmail.com>
2024-04-14 16:52:19 +0300

    Add photolib-add-mod-dates.sh script

---
+++ photolib-add-mod-dates.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+shopt -s nocaseglob
+
+for filename in *.jpg; do
+    echo "$filename $(stat -f '%Sm' -t '%Y-%m-%d %H-%M-%S' "$filename")"
+    exiftool -Alldates="$(stat -f '%Sm' -t '%Y-%m-%d %H-%M-%S' "$filename")" "$filename"
+done
+
+shopt -u nocaseglob
